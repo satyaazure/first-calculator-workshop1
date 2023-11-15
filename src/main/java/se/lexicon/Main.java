ec83314c56980c12b3bac6e1990cf74dca8431d3
satyaazure <bindumadhavib1992@gmail.com>
2023-11-15 12:00:59 +0100

    second

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -1,19 +1,20 @@
 package se.lexicon;
+import java.util.Scanner;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        Scanner scanner = new Scanner(System.in);
+        boolean runApp = true;
+        int num1;
+        int num2;
+        String operation;
+        System.out.println("Enter number");
+        num1 = scanner.nextInt();
+        System.out.println("Enter second number");
+        num2 = scanner.nextInt();
+        System.out.println("choose operations");
+        System.out.println("1 : Addition \n 2 : Subtraction \n 3 : Multiply \n 4 : Divide \n 0 : Exit");
         }
     }
-}
